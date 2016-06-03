745857f4763779a675bdc4b1aa1488ada7c42a70
Elias Silva <eliasmanuelsilva@hotmail.com>
2016-06-03 16:35:07 +0100

    Menu inicial

--- GTA0/src/org/academia/gta/Game.java
+++ GTA0/src/org/academia/gta/Game.java
@@ -1,5 +1,7 @@
 package org.academia.gta;
 
+import org.academia.gta.controls.PlayerControls;
+import org.academia.gta.controls.MouseControl;
 import org.academia.gta.gameobject.Bullet;
 import org.academia.gta.gameobject.GameObject;
 import org.academia.gta.gameobject.GameObjectFactory;
@@ -24,6 +26,8 @@ public class Game {
      */
     private Grid grid;
     private PropsGenerator propsGenerator;
+    private PlayerControls playerControls;
+    private MouseControl mouse;
 
     /**
      * Animation delay
@@ -33,17 +37,58 @@ public class Game {
     private int width;
     private int height;
 
+
     Game(int width, int height, int delay) {
 
         grid = new SimpleGfxGrid();
         this.delay = delay;
         this.width = width;
         this.height = height;
+        mouse = new MouseControl();
 
     }
 
     LinkedList<Bullet> bulletsInstantiated = new LinkedList<>();
     LinkedList<GameObject> gameObjectInstantiated = new LinkedList<>();
+
+    public void start() throws InterruptedException {
+        Picture background = new Picture(0, 0, "resources/background_gta_rambo_start2.jpg");
+        Picture logo = new Picture(225, 100, "resources/gta_rambo_logo.png");
+        Picture start = new Picture(370, 400, "resources/bt_start_normal.png");
+        Picture mouseOver = new Picture(370, 400, "resources/bt_start_over.png");
+
+        background.draw();
+        logo.draw();
+        start.draw();
+
+        while (true) {
+
+            if (mouse.getMouseXmove() > 370 && mouse.getMouseXmove() < 570 &&
+                    mouse.getMouseYmove() > 400 && mouse.getMouseYmove() < 480) {
+                start.delete();
+                mouseOver.draw();
+                if (/*playerControls.isShooted() &&*/ mouse.getMouseXclicked() > 370 && mouse.getMouseXclicked() < 570 &&
+                        mouse.getMouseYclicked() > 400 && mouse.getMouseYclicked() < 480) {
+                    //playerControls.setShooted(false);
+                    init(7, 3);
+                }
+            } else {
+                mouseOver.delete();
+                start.draw();
+            }
+
+//        if(playerControls.getMouseX() > 200 && playerControls.getMouseX() < 300 && playerControls.getMouseY() > 200 && playerControls.getMouseY() < 300) {
+//            start.delete();
+//            mouseOver.draw();
+//        } else {
+//            mouseOver.delete();
+//            start.draw();
+//        }
+
+
+        }
+
+    }
 
     /**
      * Creates a bunch of cars and randomly puts them in the field
--- GTA0/src/org/academia/gta/gameobject/people/Player.java
+++ GTA0/src/org/academia/gta/gameobject/people/Player.java
@@ -3,12 +3,9 @@ package org.academia.gta.gameobject.people;
 import org.academia.gta.controls.Direction;
 import org.academia.gta.controls.PlayerControls;
 import org.academia.gta.gameobject.Bullet;
-import org.academia.gta.gameobject.GameObjectFactory;
-import org.academia.gta.gameobject.GameObjectType;
 import org.academia.gta.representation.MovableRepresentable;
 import org.academia.gta.representation.Representable;
 import org.academia.gta.simplegfx.BulletSGFX;
-import org.academia.gta.simplegfx.SGFXRepresentationFactory;
 
 /**
  * Created by codecadet on 01/06/16.
@@ -21,6 +18,7 @@ public class Player extends Person {
     private int totalAmmo = 2;
     private int numBullets = MAX_NUM_BULLETS;
     private PlayerControls playerControls = new PlayerControls();
+
 
     private Direction currentDirection = Direction.NULL;
 
